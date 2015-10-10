95b30da155bc47d56ef4c41ec07314b2ad673b2c
zinggand <zinggand@student.ethz.ch>
2015-10-10 22:41:52 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -3,19 +3,23 @@ package jgiger.a2.vs.inf.ethz.ch.vs_jgiger_webservices;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private final String LOG_TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
 
     @Override
@@ -39,4 +43,25 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onButtonClick(View v){
+        int id = v.getId();
+
+        switch (id){
+            case (R.id.buttonTask1):
+                Log.d(LOG_TAG, "clicked on http raw button");
+                // todo: start http raw request
+            case (R.id.buttonTask2):
+                Log.d(LOG_TAG, "clicked on http library button");
+                // todo: start http raw request
+            case (R.id.buttonTask3):
+                Log.d(LOG_TAG, "clicked on json button");
+                // todo: start http raw request
+            default:
+                break;
+
+        }
+
+    }
+
 }
